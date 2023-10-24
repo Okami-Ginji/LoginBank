3e9f1e716b741f3b79f0a389698d770f30d9caf0
Administrator <Administrator@21AK22-COM>
2023-10-24 14:07:46 +0700

    Init project

---
+++ src/model/User.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+
+public class User {
+    private int AccountNumber;
+    private String Password;
+    private String Captcha;
+        
+    
+    public User(int AccountNumber, String Password, String Captcha) {
+        this.AccountNumber = AccountNumber;
+        this.Password = Password;
+        this.Captcha = Captcha;
+    }
+    
+
+    public int getAccountNumber() {
+        return AccountNumber;
+    }
+
+    public void setAccountNumber(int AccountNumber) {
+        this.AccountNumber = AccountNumber;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
+    public String getCaptcha() {
+        return Captcha;
+    }
+
+    public void setCaptcha(String Captcha) {
+        this.Captcha = Captcha;
+    }
+    
+}
